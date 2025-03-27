0e056952ecdb5856dc79a64292c34a842f0ce2b8
mazdaaudioalacaz <111906581+mazdaaudioalacaz@users.noreply.github.com>
2025-03-27 12:43:15 +0300

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="X4Mn8DSVrAq00AZo57kBm9_JP8cJha4YzYB2-65plHM" />
+      <title>Sayfanızın Başlığı</title>
+      {/* Diğer global meta etiketler */}
+    </>
+  );
+}
